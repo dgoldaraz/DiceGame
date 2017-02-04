65dd64fa96c6638cdb2b2463829b931686d33e37
dgoldaraz <dgoldaraz@gmail.com>
2017-02-04 16:18:56 +0000

    Add Impulse to Dice

--- DiceGame/Source/DiceGame/DiceComponent.cpp
+++ DiceGame/Source/DiceGame/DiceComponent.cpp
@@ -2,8 +2,13 @@
 
 #include "DiceGame.h"
 #include "DiceComponent.h"
+#include "DrawDebugHelpers.h"
+#include "RenderCore.h"
 
-
+/*
+This class encapsulates the behavior of the any Dice
+It will allow the user to throw the dice, know when it's stoped and return the value of the face up on the dice
+*/
 // Sets default values for this component's properties
 UDiceComponent::UDiceComponent()
 {
@@ -11,8 +16,6 @@ UDiceComponent::UDiceComponent()
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -20,17 +23,53 @@ UDiceComponent::UDiceComponent()
 void UDiceComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	Owner = GetOwner();
+	//Get Owner and MeshComponent
 
-	// ...
-	
+	if (Owner)
+	{
+		MeshComponent = Owner->FindComponentByClass<UStaticMeshComponent>();
+		if (MeshComponent)
+		{
+			//Desactivate Physics until the dice it's rolled
+			MeshComponent->SetSimulatePhysics(false);
+		}
+	}
+	InitialPosition = Owner->GetActorLocation();
+	InitialRotator = Owner->GetActorRotation();
 }
 
 
 // Called every frame
-void UDiceComponent::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UDiceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	DrawDebugLine(GetWorld(), Owner->GetActorLocation(), Owner->GetActorLocation() + Owner->GetActorForwardVector() * 100.0f, FColor::Green, false, 1.0f, 0, 5);
+}
 
-	// ...
+void UDiceComponent::RollDice()
+{
+	//Calculate a new Thrown direction (using min max)
+	//Add Random rotations to the dice
+	if (Owner != nullptr)
+	{
+		FVector NewOffset = FVector(FMath::RandRange(MinDirection.X, MaxDirection.X), FMath::RandRange(MinDirection.Y, MaxDirection.Y), FMath::RandRange(MinDirection.Z, MaxDirection.Z));
+		//Add the new offset to the forward direction
+		FVector NewDirection = Owner->GetActorForwardVector() + NewOffset;
+		float Impulse = FMath::RandRange(200.0f, MaxImpulse);
+		DrawDebugLine(GetWorld(), Owner->GetActorLocation(), Owner->GetActorLocation() + NewDirection * Impulse, FColor::Red, true, 10.0f, 0, 5);
+		//Add impulse
+		MeshComponent->SetSimulatePhysics(true);
+		MeshComponent->AddImpulse(NewDirection * Impulse);
+		FVector TorqueImpulse = FVector(FMath::RandRange(MinRotation.X, MaxRotation.X), FMath::RandRange(MinRotation.Y, MaxRotation.Y), FMath::RandRange(MinRotation.Z, MaxRotation.Z));
+		MeshComponent->AddAngularImpulse(TorqueImpulse * Impulse);
+	}
+}
+
+void UDiceComponent::ResetPosition()
+{
+	Owner->SetActorLocation(InitialPosition);
+	MeshComponent->SetSimulatePhysics(false);
+	Owner->SetActorRotation(InitialRotator);
 }
 
--- DiceGame/Source/DiceGame/DiceComponent.h
+++ DiceGame/Source/DiceGame/DiceComponent.h
@@ -11,16 +11,47 @@ class DICEGAME_API UDiceComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Dice")
+	void RollDice();
+
+	//Reset Position to Initial
+	UFUNCTION(BlueprintCallable, Category = "Dice")
+	void ResetPosition();
+
+	//Set the min and max direction between the dice throw direction will be calculated
+	UPROPERTY(EditAnywhere, Category = "Dice")
+	FVector MinDirection = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, Category = "Dice")
+	FVector MaxDirection = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, Category = "Dice")
+	float MaxImpulse = 500.0f;
+
+
+	UPROPERTY(EditAnywhere, Category = "Dice")
+	FVector MinRotation = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, Category = "Dice")
+	FVector MaxRotation = FVector::ZeroVector;
+
+
 	// Sets default values for this component's properties
 	UDiceComponent();
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
-	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+private:
+
+	AActor* Owner = nullptr;
+	UStaticMeshComponent* MeshComponent = nullptr;
+	FVector InitialPosition = FVector::ZeroVector;
+	FRotator InitialRotator = FRotator::ZeroRotator;
 };
